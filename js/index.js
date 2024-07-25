668f74bb62f5a1101381171b040043935e621fb9
marcosariel <marcos-olivera@outlook.es>
2024-07-25 17:16:45 -0300

    carga

---
+++ js/index.js
@@ -0,0 +1,10 @@
+//document.addEventListener('DOMContentLoaded', () => {
+  //  pintarProductos(productos);
+  //  pintarCarrito(carrito);
+ //   actualizarTotalCarrito(carrito);
+ // });
+
+
+ document.addEventListener('DOMContentLoaded', pintarProductos(productos));
+
+ document.addEventListener('DOMContentLoaded', pintarCarrito(carrito));
---
+++ js/modal.js
@@ -0,0 +1,18 @@
+//const myModal = document.getElementById('myModal')
+//const myInput = document.getElementById('myInput')
+
+//myModal.addEventListener('shown.bs.modal', () => {
+//  myInput.focus()
+//})
+
+const modalCarrito = document.querySelector('.modal-content');
+
+modalCarrito.addEventListener('click', (event) => {
+    if (event.target.classList.contains('botonEliminar')) {
+        eliminarProductoCarrito(event.target.value);
+    };
+
+});
+
+
+
